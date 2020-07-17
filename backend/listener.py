64051f6a57c527a52c8f45e20b65b219fb68960c
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2020-07-17 09:53:46 -0500

    reformat listener class on_status

--- backend/listener.py
+++ backend/listener.py
@@ -6,6 +6,7 @@
 """
 
 from tweepy.streaming import StreamListener
+from tweepy.api import API
 import json
 import time
 import sys
@@ -16,22 +17,34 @@ class Listener(StreamListener):
     """ 
     A listener that handles tweets received from the stream.
     """
-    def __init__(self, api):
-        self.api = api
+    def __init__(self, api = None):
+        self.api = api or API(wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
         self.reconnection_attemps = 0
         self.collected_tweets = 0
 
-    def on_data(self, data):
+        # create a engine to the database
+        # self.engine = create_engine('sqlite:///app/tweets.sqlite')
+        # switch to the following definition if run this code locally
+        self.engine = create_engine('sqlite:///tweets.sqlite')
+
+    def on_status(self, status):
+        """Called when a new status arrives
+        Recieves a Status object. Tweet data is in Status._json
+        """
 
         self.reconnection_attemps = 0       # restart reconnection attemps counter when there is incoming data
         self.collected_tweets += 1
 
-        tweet = json.loads(data)
-        print(self.collected_tweets,'\t',tweet['created_at'], end='\r')
+        tweet = json.loads(status._json)
+        print(self.collected_tweets,'\t',tweet['created_at'],'\t',tweet['text'])
 
         return True
 
     def on_error(self, status):
+        """Called when a non-200 status code is returned
+        Handles rate limit, increses reconnection attempt time every time is unsuccessful.
+        """
+        
         if status == 420:
             self.reconnection_attemps += 1
             sys.stdout.write('Error 420: Enhance Your Calm, the app has been rate limited\n')
